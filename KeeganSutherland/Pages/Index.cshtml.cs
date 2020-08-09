d7de5913fcbca914dd16afd760b32c15ad35a01c
Keegan Sutherland <keegan94@gmail.com>
2020-08-09 16:30:37 -0400

    Removed under construction page and added more details to the page

--- KeeganSutherland/Pages/Index.cshtml.cs
+++ KeeganSutherland/Pages/Index.cshtml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace KeeganSutherland.Pages
@@ -19,7 +14,7 @@ namespace KeeganSutherland.Pages
 
         public void OnGet()
         {
-            if (HttpContext.Request.Query["password"] != "Alia") Response.Redirect("/UnderConstruction");
+            //if (HttpContext.Request.Query["password"] != "Alia") Response.Redirect("/UnderConstruction");
         }
     }
 }
